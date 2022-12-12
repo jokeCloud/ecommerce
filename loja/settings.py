e6130422c5114af9ae2121ee54bb162a21135489
jokecloud <medalhaemerson@gmail.com>
2022-12-12 12:50:15 -0300

    created models

--- loja/settings.py
+++ loja/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'produto',
+    'pedido',
+    'perfil',
 ]
 
 MIDDLEWARE = [
--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import ItemPedido, Pedido
+
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemPedidoInline,
+    ]
+
+
 # Register your models here.
+admin.site.register(Pedido)
+admin.site.register(ItemPedido)
